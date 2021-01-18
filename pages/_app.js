25e8ba4bdd8a1c5c23f0960fffdfeb10ba962fbf
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-01-18 11:03:32 -0300

    Responsive fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../sass/base.sass";
+import "../sass/Card.sass";
+import "../sass/Form.sass";
+import "../sass/Copyright.sass";
+import "../sass/MediaQueries.sass";
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
