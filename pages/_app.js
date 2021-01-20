dd9bbdc039b75643f9de24d1f221ce4f8cb2a629
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-01-20 14:30:34 -0300

    Info box redesign & responsive fixes

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import "../sass/Card.sass";
 import "../sass/Form.sass";
 import "../sass/Copyright.sass";
 import "../sass/MediaQueries.sass";
+import 'render-smooth-image-react/build/style.css';
 
 
 function MyApp({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from "next/head";
 import { useState } from "react";
 import { motion } from "framer-motion";
+import RenderSmoothImage from "render-smooth-image-react";
 
 // Icons
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import SendIcon from "@material-ui/icons/Send";
 
 const variants1 = {
   hidden: { y: 20, opacity: 0 },
@@ -13,6 +15,14 @@ const variants1 = {
     transition: { delay: 0.1, when: "beforeChildren" },
   },
   exit: { y: 20, opacity: 0 },
+};
+
+const variants0 = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: { when: "beforeChildren" },
+  },
 };
 
 export default function Home() {
@@ -25,7 +35,12 @@ export default function Home() {
       <Head>
         <title>Girl, andá a terapia</title>
       </Head>
-      <div className="app">
+      <motion.div
+        variants={variants0}
+        initial="hidden"
+        animate="visible"
+        className="app"
+      >
         <motion.div
           variants={variants1}
           initial="hidden"
@@ -34,10 +49,10 @@ export default function Home() {
         >
           <motion.div variants={variants1} className="info">
             <motion.div variants={variants1} className="oiga-podcast">
-              <img src="/img/oigaPodcast.png" alt="" />
+              <RenderSmoothImage src={"/img/oigaPodcast.png"} />
             </motion.div>
             <motion.div variants={variants1} className="logo">
-              <img src="/img/Logo.png" alt="" />
+              <RenderSmoothImage src={"/img/Logo.png"} />
             </motion.div>
             <div className="links">
               <div className="link">
@@ -100,24 +115,27 @@ export default function Home() {
               <textarea
                 placeholder="Testimonio"
                 name="testimonio"
-                id=""
-                cols="30"
-                rows="10"
+                id="testimonio"
               ></textarea>
             </div>
-            <div className="submit">Enviar</div>
+            <div className="submit">
+              <SendIcon />
+              Enviar
+            </div>
           </motion.div>
         </motion.div>
         <div className="copyright">
           <h4>Crafted with </h4>
-          <img src="/img/heart-rainbow.png" alt="" />
+          <div className="heart">
+            <RenderSmoothImage src={"/img/heart-rainbow.png"} />
+          </div>
           <h4>
             <a target="_blank" href="http://joaquinsant.ar">
               by Joaquín Santarcángelo
             </a>
           </h4>
         </div>
-      </div>
+      </motion.div>
     </>
   );
 }
